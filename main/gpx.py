c99357c9661c06a315e28f5c98b5a61bcb6488b8
Loren M. Lang <lorenl@north-winds.org>
2015-06-26 17:56:12 -0700

    Initial loading of actual GPX files now happening

--- main/gpx.py
+++ main/gpx.py
@@ -4,17 +4,39 @@ class GPXError(Exception):
 	pass
 
 class GPX:
-	trackCount = 0
 	author = ''
+	name = ''
+	tracks = []
+
+	# TODO Refractor XML extraction of text
+	# TODO Add more GPX metadata
+	def parseGPX(self, root):
+		ns = { 'gpx': 'http://www.topografix.com/GPX/1/1' }
+		if root.tag == 'gpx':
+			ns = { 'gpx': '' }
+		element = root.find('gpx:metadata/gpx:author/gpx:name', ns)
+		if element != None:
+			self.author = element.text
+		element = root.find('gpx:metadata/gpx:name', ns)
+		if element != None:
+			self.name = element.text
+		self.tracks = root.findall('gpx:trk', ns)
 
 	def loadString(self, string):
 		#if len(string) == 0:
 		#	raise GPXError()
 		try:
-			doc = etree.fromstring(string)
+			root = etree.fromstring(string)
 		except etree.LxmlError as ex:
-			print 'Food: ' + str(ex)
-			raise GPXError('hi')
+			raise GPXError(ex)
+
+		self.parseGPX(root)
 
 	def loadFile(self, file):
-		self.author = 'Loren M. Lang'
+		#self.author = 'Anonymous'
+		try:
+			doc = etree.parse(file)
+		except etree.LxmlError as ex:
+			raise GPXError(ex)
+
+		self.parseGPX(doc.getroot())
--- main/tests.py
+++ main/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 from main.models import Track
-from main.gpx import GPX, GPXException
+from main.gpx import GPX, GPXError
 
 class TrackModelTestCase(TestCase):
 	def setup(self):
@@ -16,38 +16,65 @@ class TrackModelTestCase(TestCase):
 		Track.objects.create()
 
 class GPXTestCase(TestCase):
+	# TODO Refractor to move all common asserts to helper function
+	# TODO Fix dependency on hard-coded file paths
 	def test_load_gpx_string(self):
 		gpx = GPX()
 		gpx.loadString('<gpx/>')
-		self.assertEqual(gpx.trackCount, 0)
 		self.assertEqual(gpx.author, '')
+		self.assertEqual(gpx.name, '')
+		self.assertEqual(len(gpx.tracks), 0)
 
 	def test_load_empty_file(self):
 		gpx = GPX()
-		with self.assertRaises(GPXException):
+		with self.assertRaises(GPXError):
 			gpx.loadString('')
 
 	def test_load_malformed_xml(self):
 		gpx = GPX()
-		with self.assertRaises(GPXException):
+		with self.assertRaises(GPXError):
 			gpx.loadString('<gpx><metadata></gpx>')
 
-	@skip("Not implemented yet")
 	def test_load_gpx_string_minimal(self):
 		gpx = GPX()
 		gpx.loadString('<gpx><metadata><name>GPS</name></metadata></gpx>')
-		self.assertEqual(gpx.trackCount, 0)
-		self.assertEqual(gpx.name, '')
+		self.assertEqual(gpx.author, '')
+		self.assertEqual(gpx.name, 'GPS')
+		self.assertEqual(len(gpx.tracks), 0)
+
+	def test_load_gpx_string_full_metadata(self):
+		gpx = GPX()
+		gpx.loadString('<gpx><metadata><name>GPS</name><author><name>Jimmy</name></author></metadata></gpx>')
+		self.assertEqual(gpx.author, 'Jimmy')
+		self.assertEqual(gpx.name, 'GPS')
+		self.assertEqual(len(gpx.tracks), 0)
+
+	# XXX Do I really care able duplicates when it fails reasonably?
+	#def test_load_gpx_duplicate_metadata(self):
+	#	gpx = GPX()
+	#	gpx.loadString('<gpx><metadata><name>GPS</name></metadata><metadata><name>GPS4</name></metadata></gpx>')
+	#	self.assertEqual(gpx.author, '')
+	#	self.assertEqual(gpx.name, 'GPS')
+	#	self.assertEqual(len(gpx.tracks), 0)
+
+	def test_load_gpx_string_one_track(self):
+		gpx = GPX()
+		gpx.loadString('<gpx><metadata><name>Fun</name></metadata><trk/></gpx>')
+		self.assertEqual(gpx.author, '')
+		self.assertEqual(gpx.name, 'Fun')
+		self.assertEqual(len(gpx.tracks), 1)
 
 	def test_load_gpx_file(self):
 		gpx = GPX()
-		gpx.loadFile('testdata/simple.gpx')
-		self.assertEqual(gpx.trackCount, 0)
-		self.assertEqual(gpx.author, 'Loren M. Lang')
+		gpx.loadFile('main/testdata/simple.gpx')
+		self.assertEqual(gpx.author, 'Anonymous')
+		self.assertEqual(gpx.name, 'First test')
+		self.assertEqual(len(gpx.tracks), 0)
 
-	@skip("Not implemented yet")
+	#@skip("Not implemented yet")
 	def test_load_gpx_file2(self):
 		gpx = GPX()
-		gpx.loadFile('testdata/simple2.gpx')
-		self.assertEqual(gpx.trackCount, 0)
+		gpx.loadFile('main/testdata/simple2.gpx')
 		self.assertEqual(gpx.author, 'John Doe')
+		self.assertEqual(gpx.name, 'Second test')
+		self.assertEqual(len(gpx.tracks), 1)
